20af7ad3c00e2637fe676eb0ac94637147b3273d
Aarushi Gupta <aarushigupta55555@gmail.com>
2020-05-21 16:09:19 +0530

    Create basic algorithm

---
+++ BSAlgo.py
@@ -0,0 +1,28 @@
+def binary_search(list1, target1):
+    lower = 0
+    upper = len(list1) - 1
+    while lower <= upper:
+        mid = (upper + lower) // 2
+        if list1[mid] < target1:
+            lower = mid + 1
+        elif list1[mid] > target1:
+            upper = mid - 1
+        else:
+            return mid
+    return -1
+
+
+length = int(input("Enter the length of the list: "))
+list1 = []
+for i in range(0, length):
+    element = int(input(f"Enter the element at the index {i}: "))
+    list1.append(element)
+list1.sort()
+print(list1)
+x = int(input("Enter the target value: "))
+result = binary_search(list1, x)
+
+if result != -1:
+    print(f"Element is present at index {(result)} of the list {list1}")
+else:
+    print("Element is not present in array")
